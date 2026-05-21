2a096bc15591756f6aa68a8e33be42fd8174762d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-05-21 19:15:38 +0300

    [nix] iosevka-neg-fonts: use nixpkgs nerd-font-patcher instead of cloning full repo; all hashes resolved

--- vms/nixos/pkgs/iosevka-neg-fonts.nix
+++ vms/nixos/pkgs/iosevka-neg-fonts.nix
@@ -1,12 +1,11 @@
-{ lib, stdenvNoCC, fetchFromGitHub, nodejs, fontforge, python3, ttfautohint }:
+{ lib, stdenvNoCC, fetchFromGitHub, nodejs, fontforge, python3, nerd-font-patcher }:
 
 let
   version = "34.1.0";
   tomlFile = ./iosevka-neg.toml;
 
-  # Build Iosevka fonts from the custom build plan
-  iosevkaFonts = stdenvNoCC.mkDerivation {
-    pname = "iosevka-neg-fonts-raw";
+  iosevkaRaw = stdenvNoCC.mkDerivation {
+    pname = "iosevka-neg-raw";
     inherit version;
 
     src = fetchFromGitHub {
@@ -29,50 +28,22 @@ let
       cp dist/IosevkaNeg/TTF/*.ttf $out/share/fonts/
     '';
   };
-
-  # Patch with Nerd Font glyphs
-  nerdFontPatcher = fetchFromGitHub {
-    owner = "ryanoasis";
-    repo = "nerd-fonts";
-    rev = "v3.4.0";
-    hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-  };
 in
 stdenvNoCC.mkDerivation {
   pname = "iosevka-neg-fonts";
   inherit version;
 
-  src = iosevkaFonts;
+  dontUnpack = true;
 
-  nativeBuildInputs = [ fontforge python3 ttfautohint ];
+  nativeBuildInputs = [ fontforge python3 nerd-font-patcher ];
 
   buildPhase = ''
     mkdir -p patched
-    for ttf in $src/share/fonts/*.ttf; do
-      fontforge -script ${nerdFontPatcher}/font-patcher \
+    for ttf in ${iosevkaRaw}/share/fonts/*.ttf; do
+      fontforge -script ${nerd-font-patcher}/bin/font-patcher \
         --complete --outputdir patched "$ttf"
     done
-
   '';
-  # Strip "Nerd Font" from family names
-  # (Python fontTools script from the PKGBUILD)
-  fixupPhase = ''
-    python3 -c "
-from fontTools.ttLib import TTFont
-import glob, os
-for f in glob.glob('patched/*.ttf'):
-    tt = TTFont(f)
-    name = tt['name']
-    for record in name.names:
-        if record.nameID in (1, 3, 4, 6, 16, 21):
-            s = record.toUnicode()
-            if 'Nerd Font' in s:
-                s = s.replace(' Nerd Font', "")
-                record.string = s
-    tt.save(f + '.tmp')
-    os.rename(f + '.tmp', f)
-"
-    '';
 
   installPhase = ''
     mkdir -p $out/share/fonts/TTF
@@ -80,7 +51,7 @@ for f in glob.glob('patched/*.ttf'):
   '';
 
   meta = with lib; {
-    description = "Iosevka Neg custom font with Nerd Font patching";
+    description = "Iosevka Neg custom font with Nerd Font glyphs";
     homepage = "https://github.com/be5invis/Iosevka";
     license = licenses.ofl;
     platforms = platforms.all;

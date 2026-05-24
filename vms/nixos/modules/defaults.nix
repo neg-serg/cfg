e6ea8a0d946ffa437decf0e8dadb2b4079617b4d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-05-25 02:05:29 +0300

    [feat] enable all modules for full VM test
    Enable containers, proxy, proxypilot, installers for full
    end-to-end deployment testing in VM.

--- vms/nixos/modules/defaults.nix
+++ vms/nixos/modules/defaults.nix
@@ -6,20 +6,20 @@
   _desktop.enable = lib.mkDefault true;
   _audio.enable = lib.mkDefault true;
   _network.enable = lib.mkDefault true;
-  _containers.enable = lib.mkDefault false;
+  _containers.enable = lib.mkDefault true;
   _ai.enable = lib.mkDefault true;
   _monitoring.enable = lib.mkDefault true;
   _steam.enable = lib.mkDefault true;
   _dev.enable = lib.mkDefault true;
-  _proxy.enable = lib.mkDefault false;
+  _proxy.enable = lib.mkDefault true;
 
   # Phase 1 migration modules
   _flatpak.enable = lib.mkDefault true;
   _mpd.enable = lib.mkDefault true;
-  _proxypilot.enable = lib.mkDefault false;
+  _proxypilot.enable = lib.mkDefault true;
   _espanso.enable = lib.mkDefault true;
   _userServices.enable = lib.mkDefault true;
-  _installers.enable = lib.mkDefault false;
+  _installers.enable = lib.mkDefault true;
 
   # VM testing: auto-login for SPICE testing (skip greeter)
   _desktop.autoLogin = lib.mkDefault true;

e18e3d910dbdff639f348a8bf0cc2550ccd0926d
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-05-25 10:07:36 +0300

    [fix] MPD: port expects int not string

--- vms/nixos/modules/mpd.nix
+++ vms/nixos/modules/mpd.nix
@@ -17,7 +17,7 @@ in
         playlist_directory = "/home/neg/.config/mpd/playlists";
         db_file = "/home/neg/.local/share/mpd/database";
         bind_to_address = "127.0.0.1";
-        port = "6600";
+        port = 6600;
         audio_output = [
           { name = "PipeWire"; type = "pipewire"; }
           {

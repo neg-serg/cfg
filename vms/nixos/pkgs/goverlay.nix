b1d332dbfe7d99a9ada8f7cbd2290ace7583ddb1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-05-28 15:11:03 +0300

    [build] goverlay switched to real upstream benjamimgois/goverlay
    Source hash: sha256-xqA6FfPfQPZ+aDASjAKMBI1T8Ez0sH8qJ9+Ve0PyUZY=
    17 remaining personal packages marked as local-only (no public repo).
    Fixed buildInputs: gtk3 + glib + meson (was fetchurl placeholder).

--- vms/nixos/pkgs/goverlay.nix
+++ vms/nixos/pkgs/goverlay.nix
@@ -1,25 +1,23 @@
-{ lib, stdenv, fetchurl, autoPatchelfHook, makeWrapper }:
+{ lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, glib, gtk3, wrapGAppsHook3 }:
 
 stdenv.mkDerivation rec {
   pname = "goverlay";
-  version = "1.3.0";
+  version = "1.3";
 
-  src = fetchurl {
-    url = "https://github.com/neg-serg/goverlay/releases/download/v${version}/goverlay-x86_64-unknown-linux-gnu.tar.gz";
-    hash = "";
+  src = fetchFromGitHub {
+    owner = "benjamimgois";
+    repo = "goverlay";
+    rev = version;
+    hash = "sha256-xqA6FfPfQPZ+aDASjAKMBI1T8Ez0sH8qJ9+Ve0PyUZY=";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook makeWrapper ];
-  sourceRoot = ".";
-
-  installPhase = ''
-    install -Dm755 goverlay $out/bin/goverlay
-  '';
+  nativeBuildInputs = [ meson ninja pkg-config wrapGAppsHook3 ];
+  buildInputs = [ glib gtk3 ];
 
   meta = with lib; {
     description = "GUI for configuring MangoHud and vkBasalt";
-    homepage = "https://github.com/neg-serg/goverlay";
-    license = licenses.mit;
+    homepage = "https://github.com/benjamimgois/goverlay";
+    license = licenses.gpl3Only;
     mainProgram = "goverlay";
     platforms = platforms.linux;
   };

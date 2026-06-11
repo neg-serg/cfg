456ae92ff06653a9df4976c9797064c16b99b6b1
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-06-11 08:24:37 +0300

    [feat] guix: production vm-minimal.scm with 242 verified packages (pulled guix, pypi-uri fixes, service fixes)

--- guix/vm-minimal.scm
+++ guix/vm-minimal.scm
@@ -1,83 +1,177 @@
-;; Minimal VM config for fast build — SSH + core tools only.
-;; Full package parity deployed via guix system reconfigure after boot.
+;; Production VM config — full toolset for neg's workstation
 (use-modules (gnu) (guix) (srfi srfi-1))
-(use-service-modules desktop networking ssh xorg spice shepherd dns)
-(use-package-modules admin base bootloaders certs curl disk file
-                     gnupg linux package-management rsync ssh shells
-                     version-control vim wget)
-
+(use-service-modules desktop networking ssh xorg spice shepherd)
+(use-package-modules admin audio base bootloaders certs compression
+           curl disk file fonts gnupg linux mpd networking
+           package-management rsync ssh shells
+           version-control vim wget xdisorg xorg)
+(use-modules ((gnu packages shells) #:select (zsh)))
 (operating-system
-  (host-name "guix-eval")
-  (timezone "Europe/Moscow")
-  (locale "en_US.utf8")
-
-  (keyboard-layout
-    (keyboard-layout "us,ru"
-                     #:options '("grp:alt_shift_toggle")))
-
-  (bootloader (bootloader-configuration
-    (bootloader grub-bootloader)
-    (targets '("/dev/vda"))
-    (terminal-outputs '(console))))
-
-  (file-systems (cons (file-system
-    (mount-point "/")
-    (device (file-system-label "Guix_image"))
-    (type "ext4"))
-    %base-file-systems))
-
-  (users (cons* (user-account
-    (name "neg") (comment "neg")
-    (password (crypt "neg"
-                     "$6$rounds=4096$SxT0QXvizdX0biWn"))
-    (group "users")
-    (supplementary-groups '("wheel" "netdev" "audio" "video" "kvm" "input"))
-    (shell (file-append zsh "/bin/zsh")))
-    %base-user-accounts))
-
-  (sudoers-file (plain-file "sudoers" "\
+ (host-name "guix-eval")
+ (timezone "Europe/Moscow")
+ (locale "en_US.utf8")
+ (keyboard-layout
+  (keyboard-layout "us,ru"
+           #:options '("grp:alt_shift_toggle")))
+ (bootloader (bootloader-configuration
+  (bootloader grub-bootloader)
+  (targets '("/dev/vda"))
+  (terminal-outputs '(console))))
+ (file-systems (cons (file-system
+  (mount-point "/")
+  (device (file-system-label "Guix_image"))
+  (type "ext4"))
+  %base-file-systems))
+ (users (cons* (user-account
+  (name "neg") (comment "neg")
+  (password (crypt "neg"
+           "$6$rounds=4096$SxT0QXvizdX0biWn"))
+  (group "users")
+  (supplementary-groups '("wheel" "netdev" "audio" "video" "kvm" "input"))
+  (shell (file-append zsh "/bin/zsh")))
+  %base-user-accounts))
+ (sudoers-file (plain-file "sudoers" "\
 root ALL=(ALL) ALL
 %wheel ALL=NOPASSWD: ALL\n"))
-
-  (packages
-    (append
-      (specifications->packages
-        (list
-          ;; Shell & core
-          "zsh" "git" "neovim" "tmux" "curl" "wget"
-          "htop" "btop" "rsync" "ripgrep" "fd" "bat"
-          "jq" "tree" "less" "which"
-          ;; SSH/network
-          "openssh" "nmap" "socat" "iperf" "lsof"
-          "tcpdump" "traceroute" "mtr"
-          ;; Build tools (needed for guix pull)
-          "gcc-toolchain" "make" "pkg-config"
-          ;; Admin
-          "strace" "lvm2" "xfsprogs" "dosfstools"
-          "btrfs-progs" "smartmontools"
-          ;; Archiving
-          "gzip" "xz" "unzip" "tar"
-          ;; Editor
-          "vim" "nano"))
-      %base-packages))
-
-  (services
-    (cons* 
-      (service openssh-service-type
-        (openssh-configuration
-          (port-number 22)
-          (password-authentication? #t)))
-      (service dhcp-client-service-type)
-      (modify-services (remove
-                         (lambda (svc)
-                           (memq (service-kind svc)
-                                 (list gdm-service-type)))
-                         %base-services)
-        (guix-service-type config =>
-          (guix-configuration
-            (inherit config)
-            (substitute-urls
-              (list "https://mirror.yandex.ru/mirrors/guix/"
-                    "https://bordeaux.guix.gnu.org"))
-            (extra-options
-              (list "--cores=8" "--max-jobs=4"))))))))
+ (packages
+  (append
+   (specifications->packages
+    (list
+     ;; Shell & Terminal
+     "zsh" "git" "neovim" "tmux" "bat" "fd" "ripgrep"
+     "fzf" "zoxide" "direnv" "abduco" "dash"
+      "cowsay" "figlet" "lolcat"
+     "chafa" 
+     ;; System Monitoring
+     "btop" "htop" "atop" "iotop" "powertop"
+     "ncdu" "progress" "pv"
+     "strace" "lsof" "sysstat" "cpupower"
+     "hwinfo" "lshw" "inxi" "smartmontools"
+     "fio" "memtester" "plocate" "inotify-tools"
+     ;; Network & Connectivity
+     "curl" "wget" "wget2" "aria2"
+     "rsync" "rclone" "syncthing"
+     "openssh" "sshfs" "sshpass" "socat"
+     "nmap" "tcpdump" "traceroute" "fping" "mtr"
+     "dnsmasq" 
+     "wireguard-tools" "whois"
+     "avahi" "nss-mdns" 
+     ;; Development
+     "python" "python-pip"
+     "rust" "go" "node" "ruby"
+     "make" "gcc-toolchain" "expect" "patchelf"
+     "cmake" "meson" "ninja" "pkg-config"
+     "gdb" "valgrind"
+          "fennel"
+     "shellcheck"  
+      "pre-commit"
+      "tree-sitter"
+     ;; Git / VCS
+     "git-lfs" "git-crypt" 
+      "tig" 
+      "subversion" 
+     ;; Editors & Terminals
+     "vim" "nano" "kate"
+     "alacritty" "foot" "kitty"  
+     ;; File Managers
+      "pcmanfm" "nautilus"
+     "tree" 
+     ;; Wayland / Compositor
+     "wl-clipboard" "wlr-randr" "wlogout" "wtype" "wofi"
+     "slurp" "grim" "swappy" 
+      "swaybg"
+     "dunst" "mako" "waybar"
+      "wf-recorder" "waypipe"
+     ;; Desktop / GNOME
+     "gnome-calculator" "gnome-calendar"
+     "gnome-characters" "gnome-clocks"
+     "gnome-console" "gnome-control-center"
+     "gnome-disk-utility" "gnome-font-viewer"
+     "gnome-keyring" "gnome-maps"
+     "gnome-music" "gnome-session" "gnome-settings-daemon"
+     "gnome-shell" 
+     "gnome-system-monitor" "gnome-text-editor"
+     "gnome-tweaks" "gnome-weather"
+     "simple-scan" "sushi" "yelp" "epiphany"
+      "evince"
+     "greetd" 
+     ;; Browsers
+     "icecat" "ungoogled-chromium-wayland"
+     ;; Media & Audio
+     "mpv" "ffmpeg" "ffmpegthumbnailer"
+     "imagemagick" "graphviz"
+     "mpd" "mpc" "beets" "picard"
+     "cava" "carla" "qpwgraph" 
+     "sox" "mediainfo" "chromaprint"
+     "playerctl" "wireplumber" "pavucontrol"
+     "gstreamer" "gst-plugins-base" "gst-plugins-good"
+     "gst-plugins-bad" "gst-plugins-ugly" "gst-libav"
+      "lsp-plugins"
+     ;; Graphics & Images
+     "gimp" "blender" "inkscape"
+     "jpegoptim" "optipng" "pngquant" 
+     "rawtherapee" "darktable"
+     ;; Documents & PDF
+     "zathura" "zathura-pdf-poppler"
+     "pandoc" 
+     ;; Archive & Compression
+     "pigz" "pbzip2" "lbzip2" "unzip" "zip"
+     "cpio" "p7zip"
+     ;; Storage & Filesystems
+     "btrfs-progs" "dosfstools" "xfsprogs"
+     "lvm2" "gptfdisk" "efibootmgr" "ddrescue" "testdisk"
+     ;; Virtualization
+     "qemu" "libvirt" "virt-manager" "virt-viewer"
+     "podman" "docker" 
+     "skopeo" "slirp4netns" 
+     ;; Security
+     "age" 
+     "password-store" "hashcat" "gnupg"
+     ;; Gaming
+     "nethack" "dosbox"
+     "mangohud" 
+     ;; Email & Productivity
+     "neomutt" "isync" "vdirsyncer"
+     "urlscan" "recoll"
+     ;; Communication
+     "telegram-desktop" 
+     "nicotine+" "transmission"
+     ;; Misc Tools
+     "jq" "jc" 
+     "hyperfine" "entr" "moreutils"
+     "rlwrap" "reptyr" "parallel"
+     "hexyl" "diff-so-fancy"
+     "tealdeer" 
+     "asciinema" "screenfetch"
+     ;; Printing
+     "cups" "system-config-printer"
+     ;; Other
+     "borg" "rclone" 
+     "streamlink" "yt-dlp"
+     "minicom" 
+     "freerdp" "remmina"
+     "fwupd" "udiskie" "upower"
+     "chezmoi" "pwgen" "parted" "sudo" "less" "which"
+     "qrencode" "stress-ng"
+      "libnotify" "xdg-utils"
+     "i3-wm" "i3status"))
+   %base-packages))
+ (services
+  (cons*
+   (service openssh-service-type
+    (openssh-configuration
+     (port-number 22)
+     (password-authentication? #t)))
+   (modify-services (remove
+             (lambda (svc)
+              (memq (service-kind svc)
+                 (list gdm-service-type)))
+             %desktop-services)
+    (guix-service-type config =>
+     (guix-configuration
+      (inherit config)
+      (substitute-urls
+       (list "https://mirror.yandex.ru/mirrors/guix/"
+          "https://bordeaux.guix.gnu.org"))
+      (extra-options
+       (list "--cores=24" "--max-jobs=8"))))))))

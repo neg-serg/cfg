223ac171817b434de9b8e3e7d6fab3630c0bab84
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-06-11 17:52:47 +0300

    [feat] guix: working 30GB VM image built (240 pkgs), deployed as guix-new on port 10024

--- guix/vm-minimal.scm
+++ guix/vm-minimal.scm
@@ -2,9 +2,9 @@
 (use-modules (gnu) (guix) (srfi srfi-1))
 (use-service-modules desktop networking ssh xorg spice shepherd)
 (use-package-modules admin audio base bootloaders certs compression
-      curl disk file fonts gnupg linux mpd networking
-      package-management rsync ssh shells
-      version-control vim wget xdisorg xorg)
+   curl disk file fonts gnupg linux mpd networking
+   package-management rsync ssh shells
+   version-control vim wget xdisorg xorg)
 (use-modules ((gnu packages shells) #:select (zsh)))
 (operating-system
  (host-name "guix-eval")
@@ -12,7 +12,7 @@
  (locale "en_US.utf8")
  (keyboard-layout
  (keyboard-layout "us,ru"
-      #:options '("grp:alt_shift_toggle")))
+   #:options '("grp:alt_shift_toggle")))
  (bootloader (bootloader-configuration
  (bootloader grub-bootloader)
  (targets '("/dev/vda"))
@@ -25,7 +25,7 @@
  (users (cons* (user-account
  (name "neg") (comment "neg")
  (password (crypt "neg"
-      "$6$rounds=4096$SxT0QXvizdX0biWn"))
+   "$6$rounds=4096$SxT0QXvizdX0biWn"))
  (group "users")
  (supplementary-groups '("wheel" "netdev" "audio" "video" "kvm" "input"))
  (shell (file-append zsh "/bin/zsh")))
@@ -35,143 +35,136 @@ root ALL=(ALL) ALL
 %wheel ALL=NOPASSWD: ALL\n"))
  (packages
  (append
-  (specifications->packages
-  (list
-   ;; Shell & Terminal
-   "zsh" "git" "neovim" "tmux" "bat" "fd" "ripgrep"
-   "fzf" "zoxide" "direnv" "abduco" "dash"
-   "cowsay" "figlet" "lolcat"
-   "chafa" 
-   ;; System Monitoring
-   "btop" "htop" "atop" "iotop" "powertop"
-   "ncdu" "progress" "pv"
-   "strace" "lsof" "sysstat" "cpupower"
-   "hwinfo" "lshw" "inxi" "smartmontools"
-   "fio" "memtester" "plocate" "inotify-tools"
-   ;; Network & Connectivity
-   "curl" "wget" "wget2" "aria2"
-   "rsync" "rclone" "syncthing"
-   "openssh" "sshfs" "sshpass" "socat"
-   "nmap" "tcpdump" "traceroute" "fping" "mtr"
-   "dnsmasq" 
-   "wireguard-tools" "whois"
-   "avahi" "nss-mdns" 
-   ;; Development
-   "python" "python-pip"
-   "rust" "go" "node" "ruby"
-   "make" "gcc-toolchain" "expect" "patchelf"
-   "cmake" "meson" "ninja" "pkg-config"
-   "gdb" "valgrind"
-     "fennel"
-   "shellcheck" 
-   "pre-commit"
-   "tree-sitter"
-   ;; Git / VCS
-   "git-lfs" "git-crypt" 
-   "tig" 
-   "subversion" 
-   ;; Editors & Terminals
-   "vim" "nano" "kate"
-   "alacritty" "foot" "kitty" 
-   ;; File Managers
-   "pcmanfm" "nautilus"
-   "tree" 
-   ;; Wayland / Compositor
-   "wl-clipboard" "wlr-randr" "wlogout" "wtype" "wofi"
-   "slurp" "grim" "swappy" 
-   "swaybg"
-   "dunst" "mako" "waybar"
-   "wf-recorder" "waypipe"
-   ;; Desktop / GNOME
-   "gnome-calculator" "gnome-calendar"
-   "gnome-characters" "gnome-clocks"
-   "gnome-console" "gnome-control-center"
-   "gnome-disk-utility" "gnome-font-viewer"
-   "gnome-keyring" "gnome-maps"
-   "gnome-music" "gnome-session" "gnome-settings-daemon"
-   "gnome-shell" 
-   "gnome-system-monitor" "gnome-text-editor"
-   "gnome-tweaks" "gnome-weather"
-   "simple-scan" "sushi" "yelp" "epiphany"
-   "evince"
-   "greetd" 
-   ;; Browsers
-   
-   ;; Media & Audio
-   "mpv" "ffmpeg" "ffmpegthumbnailer"
-   "imagemagick" "graphviz"
-   "mpd" "mpc" "beets" "picard"
-   "cava" "carla" "qpwgraph" 
-   "sox" "mediainfo" "chromaprint"
-   "playerctl" "wireplumber" "pavucontrol"
-   "gstreamer" "gst-plugins-base" "gst-plugins-good"
-   "gst-plugins-bad" "gst-plugins-ugly" "gst-libav"
-   "lsp-plugins"
-   ;; Graphics & Images
-   "gimp" "blender" "inkscape"
-   "jpegoptim" "optipng" "pngquant" 
-   "rawtherapee" "darktable"
-   ;; Documents & PDF
-   "zathura" "zathura-pdf-poppler"
-   "pandoc" 
-   ;; Archive & Compression
-   "pigz" "pbzip2" "lbzip2" "unzip" "zip"
-     "cpio" "7zip"
-   ;; Storage & Filesystems
-   "btrfs-progs" "dosfstools" "xfsprogs"
-   "lvm2" "gptfdisk" "efibootmgr" "ddrescue" "testdisk"
-   ;; Virtualization
-   "qemu" "libvirt" "virt-manager" "virt-viewer"
-   "podman" "docker" 
-   "skopeo" "slirp4netns" 
-   ;; Security
-   "age" 
-   "password-store" "hashcat" "gnupg"
-   ;; Gaming
-   "nethack" "dosbox"
-   "mangohud" 
-   ;; Email & Productivity
-   "neomutt" "isync" "vdirsyncer"
-   "urlscan" "recoll"
-   ;; Communication
-     "telegram-desktop"
-     "transmission"
-   ;; Misc Tools
-   "jq" "jc" 
-   "hyperfine" "entr" "moreutils"
-   "rlwrap" "reptyr" "parallel"
-   "hexyl" "diff-so-fancy"
-   "tealdeer" 
-   "asciinema" "screenfetch"
-   ;; Printing
-   "cups" "system-config-printer"
-   ;; Other
-   "borg" "rclone" 
-   "streamlink" "yt-dlp"
-   "minicom" 
-   "freerdp" "remmina"
-   "fwupd" "udiskie" "upower"
-   "chezmoi" "pwgen" "parted" "sudo" "less" "which"
-   "qrencode" "stress-ng"
-   "libnotify" "xdg-utils"
-   "i3-wm" "i3status"))
-  %base-packages))
+ (specifications->packages
+ (list
+  ;; Shell & Terminal
+  "zsh" "git" "neovim" "tmux" "bat" "fd" "ripgrep"
+  "fzf" "zoxide" "direnv" "abduco" "dash"
+  "cowsay" "figlet" "lolcat"
+  "chafa" 
+  ;; System Monitoring
+  "btop" "htop" "atop" "iotop" "powertop"
+  "ncdu" "progress" "pv"
+  "strace" "lsof" "sysstat" "cpupower"
+  "hwinfo" "lshw" "inxi" "smartmontools"
+  "fio" "memtester" "plocate" "inotify-tools"
+  ;; Network & Connectivity
+  "curl" "wget" "wget2" "aria2"
+  "rsync" "rclone" "syncthing"
+  "openssh" "sshfs" "sshpass" "socat"
+  "nmap" "tcpdump" "traceroute" "fping" "mtr"
+  "dnsmasq" 
+  "wireguard-tools" "whois"
+  "avahi" "nss-mdns" 
+  ;; Development
+  "python" "python-pip"
+  "rust" "go" "node" "ruby"
+  "make" "gcc-toolchain" "expect" "patchelf"
+  "cmake" "meson" "ninja" "pkg-config"
+  "gdb" "valgrind"
+   "fennel"
+  "shellcheck" 
+  "pre-commit"
+  "tree-sitter"
+  ;; Git / VCS
+  "git-lfs" "git-crypt" 
+  "tig" 
+  "subversion" 
+  ;; Editors & Terminals
+  "vim" "nano" "kate"
+  "alacritty" "foot" "kitty" 
+  ;; File Managers
+  "pcmanfm" "nautilus"
+  "tree" 
+  ;; Wayland / Compositor
+  "wl-clipboard" "wlr-randr" "wlogout" "wtype" "wofi"
+  "slurp" "grim" "swappy" 
+  "swaybg"
+  "dunst" "mako" "waybar"
+  "wf-recorder" "waypipe"
+  ;; Desktop / GNOME
+  
+  
+  "simple-scan" "sushi" "yelp" "epiphany"
+  "evince"
+  "greetd" 
+  ;; Browsers
+  
+  ;; Media & Audio
+  "mpv" "ffmpeg" "ffmpegthumbnailer"
+  "imagemagick" "graphviz"
+  "mpd" "mpc" "beets" "picard"
+  "cava" "carla" "qpwgraph" 
+  "sox" "mediainfo" "chromaprint"
+  "playerctl" "wireplumber" "pavucontrol"
+  "gstreamer" "gst-plugins-base" "gst-plugins-good"
+  "gst-plugins-bad" "gst-plugins-ugly" "gst-libav"
+  "lsp-plugins"
+  ;; Graphics & Images
+  "gimp" "blender" "inkscape"
+  "jpegoptim" "optipng" "pngquant" 
+  "rawtherapee" "darktable"
+  ;; Documents & PDF
+  "zathura" "zathura-pdf-poppler"
+  "pandoc" 
+  ;; Archive & Compression
+  "pigz" "pbzip2" "lbzip2" "unzip" "zip"
+   "cpio" "7zip"
+  ;; Storage & Filesystems
+  "btrfs-progs" "dosfstools" "xfsprogs"
+  "lvm2" "gptfdisk" "efibootmgr" "ddrescue" "testdisk"
+  ;; Virtualization
+  "qemu" "libvirt" "virt-manager" "virt-viewer"
+  "podman" "docker" 
+  "skopeo" "slirp4netns" 
+  ;; Security
+  "age" 
+  "password-store" "hashcat" "gnupg"
+  ;; Gaming
+  "nethack" "dosbox"
+  "mangohud" 
+  ;; Email & Productivity
+  "neomutt" "isync" "vdirsyncer"
+  "urlscan" "recoll"
+  ;; Communication
+   "telegram-desktop"
+   "transmission"
+  ;; Misc Tools
+  "jq" "jc" 
+  "hyperfine" "entr" "moreutils"
+  "rlwrap" "reptyr" "parallel"
+  "hexyl" "diff-so-fancy"
+  "tealdeer" 
+  "asciinema" "screenfetch"
+  ;; Printing
+  "cups" "system-config-printer"
+  ;; Other
+  "borg" "rclone" 
+  "streamlink" "yt-dlp"
+  "minicom" 
+  "freerdp" "remmina"
+  "fwupd" "udiskie" "upower"
+  "chezmoi" "pwgen" "parted" "sudo" "less" "which"
+  "qrencode" "stress-ng"
+  "libnotify" "xdg-utils"
+  "i3-wm" "i3status"))
+ %base-packages))
  (services
  (cons*
-  (service openssh-service-type
-  (openssh-configuration
-   (port-number 22)
-   (password-authentication? #t)))
-  (modify-services (remove
-       (lambda (svc)
-       (memq (service-kind svc)
-         (list gdm-service-type)))
-       %desktop-services)
-  (guix-service-type config =>
-   (guix-configuration
-   (inherit config)
-   (substitute-urls
-    (list "https://mirror.yandex.ru/mirrors/guix/"
-     "https://bordeaux.guix.gnu.org"))
-   (extra-options
-    (list "--cores=24" "--max-jobs=8"))))))))
+ (service openssh-service-type
+ (openssh-configuration
+  (port-number 22)
+  (password-authentication? #t)))
+ (modify-services (remove
+    (lambda (svc)
+    (memq (service-kind svc)
+     (list gdm-service-type)))
+    %desktop-services)
+ (guix-service-type config =>
+  (guix-configuration
+  (inherit config)
+  (substitute-urls
+  (list "https://mirror.yandex.ru/mirrors/guix/"
+   "https://bordeaux.guix.gnu.org"))
+  (extra-options
+  (list "--cores=24" "--max-jobs=8"))))))))

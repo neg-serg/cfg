7649bc1744809ed78659fb2d6568793ae2ac6d53
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-06-15 12:24:23 +0300

    [feat] VM 530 pkgs: nerdctl installed, +9 Guix packages (bpftrace curlie gnome-connections transmission etc)

--- guix/channel/custom/packages/nerdctl.scm
+++ guix/channel/custom/packages/nerdctl.scm
@@ -17,20 +17,20 @@
                (base32 "0mjgssxya1s5j8kkb6qqsv2fxadcbbvvjg7s7mgl9zjfiydl14yj"))))
     (build-system gnu-build-system)
     (arguments
-     (list #:tests? #f #:strip-binaries? #f #:validate-runpath? #f
-           #:phases #~(modify-phases %standard-phases
-                        (delete 'bootstrap) (delete 'configure)
-                        (delete 'check) (delete 'build)
-                        (delete 'patch-usr-bin-file)
-                        (delete 'patch-source-shebangs)
-                        (delete 'patch-generated-file-shebangs)
-                        (replace 'install
-                          (lambda* (#:key outputs #:allow-other-keys)
-                            (let* ((out (assoc-ref outputs "out"))
-                                   (bdir (string-append out "/bin")))
-                              (mkdir-p bdir)
-                              (copy-file "nerdctl" (string-append bdir "/nerdctl"))
-                              (chmod (string-append bdir "/nerdctl") #o555)))))))
+     '(#:tests? #f #:strip-binaries? #f #:validate-runpath? #f
+       #:phases (modify-phases %standard-phases
+                  (delete 'bootstrap) (delete 'configure)
+                  (delete 'check) (delete 'build)
+                  (delete 'patch-usr-bin-file)
+                  (delete 'patch-source-shebangs)
+                  (delete 'patch-generated-file-shebangs)
+                  (replace 'install
+                    (lambda* (#:key outputs #:allow-other-keys)
+                      (let* ((out (assoc-ref outputs "out"))
+                             (bdir (string-append out "/bin")))
+                        (mkdir-p bdir)
+                        (copy-file "nerdctl" (string-append bdir "/nerdctl"))
+                        (chmod (string-append bdir "/nerdctl") #o555)))))))
     (supported-systems '("x86_64-linux"))
     (home-page "https://github.com/containerd/nerdctl")
     (synopsis "Docker-compatible CLI for containerd")

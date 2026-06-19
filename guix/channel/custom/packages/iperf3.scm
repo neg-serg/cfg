81ddfa8ef6225350124c1cd415901747e378cf24
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-06-19 12:52:26 +0300

    [guix] archive: remove guix deployment files (system installed on bare metal)

--- guix/channel/custom/packages/iperf3.scm
+++
@@ -1,7 +0,0 @@
-(define-module (custom packages iperf3)
-  #:use-module (guix packages)
-  #:use-module (gnu packages networking))
-
-(define-public iperf3 iperf)
-
-iperf3
--- guix/guix-daemon-proxy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export http_proxy=http://127.0.0.1:10880
-export https_proxy=http://127.0.0.1:10880
-exec /var/guix/profiles/per-user/root/current-guix/bin/guix-daemon "$@"

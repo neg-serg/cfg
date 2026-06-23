cd833b27849de91cb00742884c27ed666fe08307
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-06-23 22:54:39 +0300

    [fix] nixos: container Restart=on-failure + StartLimitBurst=3 (prevents infinite restart blocking boot)

--- vms/nixos/modules/containers.nix
+++ vms/nixos/modules/containers.nix
@@ -27,8 +27,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull ollama/ollama:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name ollama -p 11434:11434 -v /var/lib/ollama:/root/.ollama ollama/ollama:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop ollama";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -41,8 +42,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull adguard/adguardhome:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name adguardhome -p 3000:3000 -p 53:53/tcp -p 53:53/udp adguard/adguardhome:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop adguardhome";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -55,8 +57,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull vaultwarden/server:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name vaultwarden -p 8080:80 -v /var/lib/vaultwarden:/data vaultwarden/server:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop vaultwarden";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -69,8 +72,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull jellyfin/jellyfin:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name jellyfin -p 8096:8096 jellyfin/jellyfin:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop jellyfin";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -83,8 +87,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull lscr.io/linuxserver/transmission:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name transmission -p 9091:9091 -p 51413:51413 lscr.io/linuxserver/transmission:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop transmission";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -97,8 +102,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull lscr.io/linuxserver/duckdns:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name duckdns lscr.io/linuxserver/duckdns:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop duckdns";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -111,8 +117,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull grafana/loki:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name loki -p 3100:3100 -v /var/lib/loki:/loki grafana/loki:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop loki";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -125,8 +132,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull grafana/grafana:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name grafana -p 3030:3000 -v /var/lib/grafana:/var/lib/grafana grafana/grafana:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop grafana";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -139,8 +147,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull ghcr.io/neg-serg/nanoclaw:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name nanoclaw ghcr.io/neg-serg/nanoclaw:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop nanoclaw";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -153,8 +162,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull grafana/promtail:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name promtail -v /var/log:/var/log:ro -v /var/lib/promtail:/etc/promtail grafana/promtail:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop promtail";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -167,8 +177,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull prom/alertmanager:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name alertmanager -p 9093:9093 prom/alertmanager:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop alertmanager";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 
@@ -181,8 +192,9 @@ in
         ExecStartPre = "${pkgs.podman}/bin/podman pull ghcr.io/huggingface/text-embeddings-inference:latest";
         ExecStart = "${pkgs.podman}/bin/podman run --rm --name llama-embed -p 8088:80 ghcr.io/huggingface/text-embeddings-inference:latest";
         ExecStop = "${pkgs.podman}/bin/podman stop llama-embed";
-        Restart = "always";
-        RestartSec = 10;
+        Restart = "on-failure";
+        RestartSec = 30;
+        StartLimitBurst = 3;
       };
     };
 

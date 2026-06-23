567fe9d95a961457077fe7451d24e6942fe72da8
Sergey Miroshnichenko <serg.zorg@gmail.com>
2026-06-23 21:58:47 +0300

    [fix] nixos: CachyOS mount noauto (systemd removes unnecessary mount jobs)

--- vms/nixos/modules/filesystems.nix
+++ vms/nixos/modules/filesystems.nix
@@ -11,7 +11,7 @@ in
     fileSystems."/mnt/cachyos" = {
       device = "/dev/nvme0n1p4";
       fsType = "xfs";
-      options = [ "nofail" "ro" ];
+      options = [ "nofail" "ro" "noauto" ];
     };
     fileSystems."/mnt/windows" = {
       device = "/dev/disk/by-uuid/86286AAE286A9CC5";
